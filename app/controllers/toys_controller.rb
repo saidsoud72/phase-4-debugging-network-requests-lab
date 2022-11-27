f90af3c88329de9b4765a22b00308ba3611366a2
saidsoud72 <said.omar@student.moringaschool.com>
2022-11-27 19:49:46 +0300

    done assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,6 +1,5 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
-
   def index
     toys = Toy.all
     render json: toys
@@ -8,12 +7,14 @@ class ToysController < ApplicationController
 
   def create
     toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy.update(toy_params)
   end
 
   def destroy
@@ -21,11 +22,9 @@ class ToysController < ApplicationController
     toy.destroy
     head :no_content
   end
-
   private
   
   def toy_params
     params.permit(:name, :image, :likes)
   end
-
 end
